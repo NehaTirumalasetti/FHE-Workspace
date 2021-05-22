ab516dc61b95c674e737f3cf06704e7780235b5f
radhamarathe <radhamarathe13@gmail.com>
2021-05-22 15:21:19 +0000

    encryption code

---
+++ encryption_test1/CMakeFiles/encryption_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encryption_test.dir/test.cpp.o"
+  "encryption_test"
+  "encryption_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encryption_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
