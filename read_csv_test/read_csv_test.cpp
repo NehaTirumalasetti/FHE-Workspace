de5247361ecb557194361b0cffe17f39c7750887
radhamarathe <radhamarathe13@gmail.com>
2021-06-01 12:14:35 +0000

    sqroot function written

--- read_csv_test/read_csv_test.cpp
+++ read_csv_test/read_csv_test.cpp
@@ -181,10 +181,10 @@ int main(int argc, char* argv[])
   
   k.encrypt(xc);
 
-  long r = 1;
+  //long r = 1;
   vector<Ctxt> bits;
-  xc.extractBits(bits, r);
-  storeinfile("bits.txt", bits);
+  //xc.extractBits(bits, r);
+  //storeinfile("bits.txt", bits);
 
   vector<vector<double>> bits_decrypt;
   for(int i =0;i<bits.size();i++)
@@ -198,6 +198,57 @@ int main(int argc, char* argv[])
   //cout<<decrytxt;
   storeinfileptxt("bits_dec.txt", bits_decrypt);
 
+  sqroot(ea, publicKey, secretKey);
 
     return 0;
+}
+
+//void sqroot(Ctxt x, int d)
+void sqroot(EncryptedArray ea, PubKey publicKey, SecKey secretKey)
+{
+  int xint = 0.5;
+  int d = 4;
+  PtxtArray one(ea, 1);
+  PtxtArray negone(ea, -1);
+  PtxtArray three(ea, 3);
+  long half = 0.5;
+  long quarter = 0.25;
+
+  PtxtArray x(ea, xint);
+  Ctxt xc(publicKey);
+  
+  x.encrypt(xc);
+
+  Ctxt a0 = xc;
+  Ctxt a1(publicKey);
+  Ctxt b0 = xc;
+  Ctxt b1(publicKey);
+  
+  b0 -= one;
+
+  for(int i=0; i<d; i++)
+  {
+    Ctxt temp1 = b0;
+    temp1.multByConstant(half);
+    temp1 -= one;
+    temp1.multByConstant(negone);
+    a0 *= temp1;
+    a1 = a0;
+
+    Ctxt temp2 = b0;
+    temp2 -= three;
+    temp2.multByConstant(quarter);
+    b0 *= b0;
+    b0 *= temp2;
+    b1 = b0;
+
+    //a0 = a1;
+    //b0 = b1;
+
+    PtxtArray dec_x (ea);
+    dec_x.decrypt(a1, secretKey);
+
+    cout << dec_x;
+  
+  }
 }
