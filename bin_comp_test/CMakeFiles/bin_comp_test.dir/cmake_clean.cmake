700893112e97e72197f75f21171881dcdc324221
NehaTirumalasetti <ntirumalasetti@gmail.com>
2021-06-24 14:55:12 +0000

    Bin comp

---
+++ bin_comp_test/CMakeFiles/bin_comp_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bin_comp_test.dir/bin_comp_test.cpp.o"
+  "bin_comp_test"
+  "bin_comp_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bin_comp_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
