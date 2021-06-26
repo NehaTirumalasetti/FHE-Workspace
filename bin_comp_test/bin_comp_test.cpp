7df41da5b855cfaacdfe1cc314b71f02b848807b
NehaTirumalasetti <ntirumalasetti@gmail.com>
2021-06-26 18:23:34 +0000

    Context working

--- bin_comp_test/bin_comp_test.cpp
+++ bin_comp_test/bin_comp_test.cpp
@@ -18,15 +18,21 @@
 using namespace std;
 using namespace helib;
 
+ long calculateLevels(bool bootstrap, long bitSize)
+  {
+    return bootstrap
+               ? 900
+               : 30 * (7 + NTL::NumBits(bitSize + 2)); // that should be enough
+  }
 
 int main(int argc, char* argv[])
 {
-  // {2, 15004, 15709, 22, 23, 683, 0, 4099, 13663, 0, 22, 31, 0, 25, 3},
+  // // {2, 15004, 15709, 22, 23, 683, 0, 4099, 13663, 0, 22, 31, 0, 25, 3},
 
-  //2, 2304, 4641, 24, 7, 3, 221, 3979, 3095, 3760, 6, 2, -8, 25, 3},
-   // { p, phi(m),   m,   d, m1, m2, m3,    g1,   g2,   g3, ord1,ord2,ord3,
-      // B,c}
-  // Plaintext prime modulus.
+  // //2, 2304, 4641, 24, 7, 3, 221, 3979, 3095, 3760, 6, 2, -8, 25, 3},
+  //  // { p, phi(m),   m,   d, m1, m2, m3,    g1,   g2,   g3, ord1,ord2,ord3,
+  //     // B,c}
+  // // Plaintext prime modulus.
   long p = 2;
   // Cyclotomic polynomial - defines phi(m).
   long m =4641;//4095; //4641
@@ -42,53 +48,91 @@ int main(int argc, char* argv[])
   std::vector<long> gens = {3979, 3095, 3760};//{2341, 3277, 911}; // 3979, 3095, 3760
   // Orders of the previous generators.
   std::vector<long> ords = { 6, 2, -8};//{6, 4, 6};// 6, 2, -8
-
- helib::Context context = helib::ContextBuilder<helib::BGV>()
-                               .m(m)
-                               .p(p)
-                               .r(r)
-                               .gens(gens)
-                               .ords(ords)
-                               .bits(bits)
-                               .c(c)
-                               .bootstrappable(true)
-                               .mvec(mvec)
-                               .build();
-
-  
-  const helib::EncryptedArray& ea = context.getEA();
-
-  helib::SecKey secret_key = helib::SecKey(context);
-  // Generate the secret key
-  secret_key.GenSecKey();
-  helib::addSome1DMatrices(secret_key);
-
-  // std::cout << "\nCreating Public Key ...";
-  const helib::PubKey& public_key = secret_key;
-   // Print the context
-  std::cout << std::endl;
-  // if (debug)
-  //   context.printout();
-
-  // Print the security level
-  // Note: This will be negligible to improve performance time.
-  //std::cout << "\n***Security Level: " << context.securityLevel()
-            //<< " *** Negligible for this example ***" << std::endl;
-
-  // Get the number of slot (phi(m))
-  long nslots = ea.size();
-  std::cout << "\nNumber of slots: " << nslots << std::endl;
-
-  static std::vector<helib::zzX> unpackSlotEncoding;
-  buildUnpackSlotEncoding(unpackSlotEncoding, context.getEA());
-  //std::vector<helib::zzX> bin_comp_test::unpackSlotEncoding;
-
-  
-  long bitSize = 6;//16
+   //Plaintext prime modulus.
+  // long p = 2;
+  // // Cyclotomic polynomial - defines phi(m).
+  // long m = 4095;
+  // // Hensel lifting (default = 1).
+  // long r = 1;
+  // // Number of bits of the modulus chain.
+  // long bits = 500;
+  // // Number of columns of Key-Switching matrix (typically 2 or 3).
+  // long c = 2;
+  // // Factorisation of m required for bootstrapping.
+  // std::vector<long> mvec = {7, 5, 9, 13};
+  // // Generating set of Zm* group.
+  // std::vector<long> gens = {2341, 3277, 911};
+  // // Orders of the previous generators.
+  // std::vector<long> ords = {6, 4, 6};
 
   bool bootstrap = false;
-  /*
-  vector<long> v {12,32,23,29,26,22,30,24,33};
+  long bitSize = 6;
+  long seed = 0;
+  long nthreads = 1;
+  long L = calculateLevels(bootstrap, bitSize);
+
+
+ helib::Context context = helib::ContextBuilder<helib::BGV>()
+                  .m(m)
+                  .p(p)
+                  .r(1)
+                  .gens(gens)
+                  .ords(ords)
+                  .buildModChain(false)
+                  .build();
+                              //  .m(m)
+                              //  .p(p)
+                              //  .r(r)
+                              //  .gens(gens)
+                              //  .ords(ords)
+                              //  .bits(bits)
+                              //  .c(c)
+                              //  .bootstrappable(true)
+                              //  .mvec(mvec)
+                              //  .build();
+   context.buildModChain(L, c, /*willBeBootstrappable=*/bootstrap);
+  // Print the context.
+  context.printout();
+  std::cout << std::endl;
+ 
+ 
+  // Print the security level.
+  std::cout << "Security: " << context.securityLevel() << std::endl;
+
+  // Secret key management.
+  std::cout << "Creating secret key..." << std::endl;
+  // Create a secret key associated with the context.
+  helib::SecKey secret_key(context);
+  // Generate the secret key.
+  secret_key.GenSecKey();
+
+  // Generate bootstrapping data.
+  //  secret_key.genRecryptData();
+  addSome1DMatrices(secret_key); // compute key-switching matrices
+  addFrbMatrices(secret_key);
+
+  // Public key management.
+  // Set the secret key (upcast: SecKey is a subclass of PubKey).
+  const helib::PubKey& public_key = secret_key;
+
+  // Get the EncryptedArray of the context.
+  const helib::EncryptedArray& ea = context.getEA();
+
+  // Build the unpack slot encoding.
+  std::vector<helib::zzX> unpackSlotEncoding;
+  buildUnpackSlotEncoding(unpackSlotEncoding, ea);
+
+  // Get the number of slot (phi(m)).
+  long nslots = ea.size();
+  std::cout << "Number of slots: " << nslots << std::endl;
+
+
+  
+  // long bitSize = 6;//16
+
+  // bool bootstrap = true;
+  
+ /* vector<long> v {12,32,23,29,26,22,30,24,33};
   long pa = NTL::RandomBits_long(bitSize);
   long pb = NTL::RandomBits_long(bitSize + 1);
   long pMax = std::max(pa, pb);
@@ -141,7 +185,10 @@ int main(int argc, char* argv[])
   cout<< "pNi " <<long(pNi) <<endl;
   cout<< "pMax "<<pMax <<endl;
   cout<< "pMin " <<pMin <<endl;*/
+  
 
+
+// vector<long> v {12,32,23,29,26,22,30,24,33};
 
  
 
